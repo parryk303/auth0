b2dd3f26f2f98dd61022b527fd8decce456d0ef7
parryk303 <kyle.parry.303@gmail.com>
2022-03-16 16:24:53 -0600

    Remove home styles from index

--- pages/api/index.jsx
+++ pages/api/index.jsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 
 import Hero from '../components/Hero';
 
 export default function Index() {
-
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Securonix SSA</title>
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
+      
       <main className={styles.main}>
-        
-       
-        
           <>
             <Hero />
             <hr />
           </>
-    
       </main>
     </div >
   );
